127709cf7a8c36c7295dc6f4b1aa25760d5e35af
Poul Kjeldager Sørensen <poul@kjeldager.com>
2020-12-19 21:26:39 +0100

    added function evaluation caching

--- src/DotNETDevOps.JsonFunctions/Configuration/ExpressionParserOptions.cs
+++ src/DotNETDevOps.JsonFunctions/Configuration/ExpressionParserOptions.cs
@@ -1,11 +1,18 @@
 ﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
 
 namespace DotNETDevOps.JsonFunctions
 {
+    public interface IExpressionFunctionCache
+    {
+        ValueTask<JToken> GetOrAdd(string key, Func<string, ValueTask<JToken>> p);
+    }
     public class ExpressionParserOptions<TContext>
     {
         public bool ThrowOnError { get; set; } = true;
         public TContext Document { get; set; }
         public bool EnableFunctionEvaluationCaching { get; set; } = false;
+        public IExpressionFunctionCache Cache { get; set; }
     }
 }
